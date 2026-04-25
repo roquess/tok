233aeb89740405737e268288b81a122ca28a600b
Steve Roques <steve.roques@gmail.com>
2026-04-25 05:19:59 +0200

    fix: propagate unsupported normalizer as error, simplify BPE stub

--- src/tok_loader.erl
+++ src/tok_loader.erl
@@ -18,53 +18,51 @@ build(Json) ->
             case maps:find(<<"type">>, Model) of
                 error       -> {error, {missing_field, <<"model.type">>}};
                 {ok, <<"WordPiece">>} -> build_wordpiece(Json, Model);
-                {ok, <<"BPE">>}      -> build_bpe(Json, Model);
                 {ok, T}              -> {error, {unsupported_tokenizer, T}}
             end
     end.
 
 build_wordpiece(Json, Model) ->
     case maps:find(<<"vocab">>, Model) of
-        error         -> {error, {missing_field, <<"model.vocab">>}};
+        error          -> {error, {missing_field, <<"model.vocab">>}};
         {ok, VocabMap} ->
-            Vocab        = maps:fold(fun(T, Id, A) -> A#{T => Id} end, #{}, VocabMap),
-            IdsToTokens  = maps:fold(fun(T, Id, A) -> A#{Id => T} end, #{}, Vocab),
-            UnkToken     = maps:get(<<"unk_token">>, Model, <<"[UNK]">>),
-            {ok, #{
-                type               => wordpiece,
-                vocab              => Vocab,
-                ids_to_tokens      => IdsToTokens,
-                special_tokens     => extract_special_tokens(Vocab),
-                normalizer         => parse_normalizer(maps:get(<<"normalizer">>, Json, null)),
-                pre_tokenizer      => parse_pre_tokenizer(maps:get(<<"pre_tokenizer">>, Json, null)),
-                max_length         => parse_max_length(Json),
-                pad_id             => parse_pad_id(Json, Vocab),
-                unk_id             => maps:get(UnkToken, Vocab, 100),
-                cls_id             => maps:get(<<"[CLS]">>, Vocab, 101),
-                sep_id             => maps:get(<<"[SEP]">>, Vocab, 102),
-                max_chars_per_word => maps:get(<<"max_input_chars_per_word">>, Model, 100)
-            }}
+            Vocab       = VocabMap,
+            IdsToTokens = maps:fold(fun(T, Id, A) -> A#{Id => T} end, #{}, Vocab),
+            UnkToken    = maps:get(<<"unk_token">>, Model, <<"[UNK]">>),
+            case parse_normalizer(maps:get(<<"normalizer">>, Json, null)) of
+                {error, _} = Err -> Err;
+                {ok, Norm}       ->
+                    {ok, #{
+                        type               => wordpiece,
+                        vocab              => Vocab,
+                        ids_to_tokens      => IdsToTokens,
+                        special_tokens     => extract_special_tokens(Vocab),
+                        normalizer         => Norm,
+                        pre_tokenizer      => parse_pre_tokenizer(maps:get(<<"pre_tokenizer">>, Json, null)),
+                        max_length         => parse_max_length(Json),
+                        pad_id             => parse_pad_id(Json, Vocab),
+                        unk_id             => maps:get(UnkToken, Vocab, 100),
+                        cls_id             => maps:get(<<"[CLS]">>, Vocab, 101),
+                        sep_id             => maps:get(<<"[SEP]">>, Vocab, 102),
+                        max_chars_per_word => maps:get(<<"max_input_chars_per_word">>, Model, 100)
+                    }}
+            end
     end.
-
-build_bpe(_Json, _Model) ->
-    {error, {unsupported_tokenizer, <<"BPE">>}}.
 
 extract_special_tokens(Vocab) ->
     Keys = [<<"[PAD]">>, <<"[UNK]">>, <<"[CLS]">>, <<"[SEP]">>, <<"[MASK]">>],
     maps:filter(fun(K, _) -> lists:member(K, Keys) end, Vocab).
 
 parse_normalizer(null) ->
-    #{clean_text => false, handle_chinese_chars => false,
-      strip_accents => false, lowercase => false};
+    {ok, #{clean_text => false, handle_chinese_chars => false,
+           strip_accents => false, lowercase => false}};
 parse_normalizer(#{<<"type">> := <<"BertNormalizer">>} = N) ->
-    #{clean_text           => maps:get(<<"clean_text">>,           N, false),
-      handle_chinese_chars => maps:get(<<"handle_chinese_chars">>, N, false),
-      strip_accents        => null_to_false(maps:get(<<"strip_accents">>, N, null)),
-      lowercase            => maps:get(<<"lowercase">>,            N, false)};
+    {ok, #{clean_text           => maps:get(<<"clean_text">>,           N, false),
+           handle_chinese_chars => maps:get(<<"handle_chinese_chars">>, N, false),
+           strip_accents        => null_to_false(maps:get(<<"strip_accents">>, N, null)),
+           lowercase            => maps:get(<<"lowercase">>,            N, false)}};
 parse_normalizer(#{<<"type">> := T}) ->
-    #{clean_text => false, handle_chinese_chars => false,
-      strip_accents => false, lowercase => false,
-      unsupported => T}.
+    {error, {unsupported_normalizer, T}}.
 
 parse_pre_tokenizer(null)                                      -> bert;
 parse_pre_tokenizer(#{<<"type">> := <<"BertPreTokenizer">>})   -> bert;
